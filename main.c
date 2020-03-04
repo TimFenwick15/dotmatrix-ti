ac90cc986e0e697fd6b92fedf753b2d990751d5f
TimFenwick15 <tjf20@hotmail.co.uk>
2020-03-04 01:56:06 +0000

    Cleanup, renamed in .project

--- main.c
+++ main.c
@@ -3,18 +3,9 @@
 #include "Library/F2802x_Examples.h"
 #include "Library/clk.h"
 
-#define REG_CONTROL // regs, over the GPIO handle stuff
-
-// With REG_CONTROL defined, I get no screen flicker, but lots of errors
-// Without, I get no errors, but some flicker. So the GPIO handle pointer control stuff must be slower than direct control
-
-#define HIGH (1)
-#define LOW (0)
-
 #define ROWS (32)
 #define ROW_ADDRESSES (ROWS / 2)
 #define WIDTH (64)
-
 #define ADDRESS_PORT_SHIFT (16)
 
 #define CLK ((Uint16)GPIO_Number_28)
@@ -31,7 +22,6 @@
 #define G2  ((Uint16)GPIO_Number_4)
 #define B2  ((Uint16)GPIO_Number_5)
 
-#ifdef REG_CONTROL
 #define CLEAR_CLK (GpioDataRegs.GPACLEAR.bit.GPIO28 = 1) // J1-3 CLK
 #define CLEAR_OE  (GpioDataRegs.GPACLEAR.bit.GPIO29 = 1) // J1-4 OE
 #define CLEAR_LAT (GpioDataRegs.GPBCLEAR.bit.GPIO34 = 1) // J1-5 LAT // Pulled up via S1-1
@@ -58,34 +48,6 @@
 #define SET_R2    (GpioDataRegs.GPASET.bit.GPIO3 = 1)
 #define SET_G2    (GpioDataRegs.GPASET.bit.GPIO4 = 1)
 #define SET_B2    (GpioDataRegs.GPASET.bit.GPIO5 = 1)
-#else
-#define CLEAR_CLK (GPIO_setLow(myGpio, GPIO_Number_28))
-#define CLEAR_OE  (GPIO_setLow(myGpio, GPIO_Number_29))
-#define CLEAR_LAT (GPIO_setLow(myGpio, GPIO_Number_34))
-#define CLEAR_A   (GPIO_setLow(myGpio, GPIO_Number_16))
-#define CLEAR_B   (GPIO_setLow(myGpio, GPIO_Number_17))
-#define CLEAR_C   (GPIO_setLow(myGpio, GPIO_Number_18))
-#define CLEAR_D   (GPIO_setLow(myGpio, GPIO_Number_19))
-#define CLEAR_R1  (GPIO_setLow(myGpio, GPIO_Number_0))
-#define CLEAR_G1  (GPIO_setLow(myGpio, GPIO_Number_1))
-#define CLEAR_B1  (GPIO_setLow(myGpio, GPIO_Number_2))
-#define CLEAR_R2  (GPIO_setLow(myGpio, GPIO_Number_3))
-#define CLEAR_G2  (GPIO_setLow(myGpio, GPIO_Number_4))
-#define CLEAR_B2  (GPIO_setLow(myGpio, GPIO_Number_5))
-#define SET_CLK   (GPIO_setHigh(myGpio, GPIO_Number_28))
-#define SET_OE    (GPIO_setHigh(myGpio, GPIO_Number_29))
-#define SET_LAT   (GPIO_setHigh(myGpio, GPIO_Number_34))
-#define SET_A     (GPIO_setHigh(myGpio, GPIO_Number_16))
-#define SET_B     (GPIO_setHigh(myGpio, GPIO_Number_17))
-#define SET_C     (GPIO_setHigh(myGpio, GPIO_Number_18))
-#define SET_D     (GPIO_setHigh(myGpio, GPIO_Number_19))
-#define SET_R1    (GPIO_setHigh(myGpio, GPIO_Number_0))
-#define SET_G1    (GPIO_setHigh(myGpio, GPIO_Number_1))
-#define SET_B1    (GPIO_setHigh(myGpio, GPIO_Number_2))
-#define SET_R2    (GPIO_setHigh(myGpio, GPIO_Number_3))
-#define SET_G2    (GPIO_setHigh(myGpio, GPIO_Number_4))
-#define SET_B2    (GPIO_setHigh(myGpio, GPIO_Number_5))
-#endif
 #define ABOUT_1S_OF_BIG_NUMBER ((Uint32)1000000) // count ~ 1us, 1s = 1,000,000 counts
 #define ABOUT_10MS_OF_BIG_NUMBER ((Uint32)10000) // count ~ 1us, 10ms = 10,000 counts
 #define ABOUT_1US_OF_BIG_NUMBER ((Uint32)1)      // count ~ 1us, 1us = 1 count
@@ -93,8 +55,6 @@
 void initTimer(void);
 void initGPIO(void);
 void setGpioAsOutput(Uint16 gpioNumber);
-void setGpioHigh(Uint16 pin);
-void setGpioLow(Uint16 pin);
 void delayMicroseconds(Uint32 delayTime);
 
 GPIO_Handle myGpio;
@@ -266,7 +226,6 @@ const Uint16 glyph_3_1[ROW_ADDRESSES] = {
  */
 int main(void)
 {
-    //Uint32 time = 0;
     // Think we need this to be able to run from flash, not during JTAG debugging
     Uint16 j = 0;
 
@@ -278,8 +237,6 @@ int main(void)
     initTimer();
 
     for (;;) {
-        //time = CpuTimer1Regs.TIM.all;
-
         SET_OE;
         SET_LAT;
 
@@ -390,8 +347,6 @@ int main(void)
             SET_CLK;
             CLEAR_CLK;
         }
-        //delayMicroseconds(100);
-        //while ((time - CpuTimer1Regs.TIM.all) < ABOUT_1US_OF_BIG_NUMBER) {};
     }
 }
 
@@ -424,9 +379,6 @@ void initGPIO(void)
     setGpioAsOutput(R2);
     setGpioAsOutput(G2);
     setGpioAsOutput(B2);
-    //setGpioAsOutput(GPIO_Number_12); // J2-3
-    //setGpioAsOutput(GPIO_Number_6);  // J2-8
-    //setGpioAsOutput(GPIO_Number_7);  // J2-9
     CLEAR_CLK;
     SET_OE;
     CLEAR_LAT;
@@ -444,25 +396,16 @@ void initGPIO(void)
 
 void initTimer(void)
 {
-    CpuTimer1Regs.TCR.bit.FREE      = 1;                /* Set CpuTimer1 to free run mode   */
-    CpuTimer1Regs.TCR.bit.SOFT      = 1;
-
-    CpuTimer1Regs.PRD.all           = 0xFFFFFFFF;       /* Initialize CpuTimer1 period to maximum   */
-
-    /* Initialize pre-scale counter to 0x7A, 1/ (150MHz / 122) = 814nS period   */
-    CpuTimer1Regs.TPR.bit.TDDR      = 0x3C;//79;             /* Set Timer1 divide down low 2 bytes with 0x79 = 0x7A - 1  */
-    CpuTimer1Regs.TPRH.bit.TDDRH    = 0x00;             /* Set Timer1 divide down high 2 bytes with 0x00            */
-
-    CpuTimer1Regs.TCR.bit.TSS       = 1;                /* Stop CpuTimer1                       */
-
-    CpuTimer1Regs.TCR.bit.TRB       = 1;                /* Reload CpuTimer1 counter register    */
-
-    CpuTimer1.InterruptCount        = 0;                /* Reset CpuTimer1 interrupt counter    */
-
-    CpuTimer1Regs.TCR.bit.TIF       = 1;                /* Clear CpuTimer1 interrupt flag       */
-
-    CpuTimer1Regs.TCR.bit.TIE       = 0;                /* Disable CpuTimer1 interrupt          */
-    CpuTimer1Regs.TIM.all           = 0xFFFFFFFF;
-
-    CpuTimer1Regs.TCR.bit.TSS       = 0;                /* Start CpuTimer1                      */
+    CpuTimer1Regs.TCR.bit.FREE = 1; // Free run mode
+    CpuTimer1Regs.TCR.bit.SOFT = 1;
+    CpuTimer1Regs.PRD.all = 0xFFFFFFFF; // Initialize CpuTimer1
+    CpuTimer1Regs.TPR.bit.TDDR = 0x3C; // 60MHz / 3C = 1us I think
+    CpuTimer1Regs.TPRH.bit.TDDRH = 0x00;
+    CpuTimer1Regs.TCR.bit.TSS = 1; // Stop CpuTimer1
+    CpuTimer1Regs.TCR.bit.TRB = 1; // Reload CpuTimer1 counter register
+    CpuTimer1.InterruptCount = 0; // Reset CpuTimer1 interrupt counter
+    CpuTimer1Regs.TCR.bit.TIF = 1; // Clear CpuTimer1 interrupt flag
+    CpuTimer1Regs.TCR.bit.TIE = 0; // Disable CpuTimer1 interrupt
+    CpuTimer1Regs.TIM.all = 0xFFFFFFFF;
+    CpuTimer1Regs.TCR.bit.TSS = 0; // Start CpuTimer1
 }
